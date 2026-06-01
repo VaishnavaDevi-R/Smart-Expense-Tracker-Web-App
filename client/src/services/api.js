32ea954bda7e122494b84eeeaa6d4f993038d02b
Vaishnava Devi <11vaishu2004@gmail.com>
2026-06-01 20:20:53 +0530

    Updated production API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://smart-expense-tracker-api-p51v.onrender.com",
 });
 
 export default API;
